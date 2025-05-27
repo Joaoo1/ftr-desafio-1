7e97fe99a1e5b8e0edafe3bd08ecb5a4b5ac83bf
João <silva.joao1@outlook.com.br>
2025-05-26 22:51:00 -0300

    feat: setup server

---
+++ server/src/infra/http/app.ts
@@ -0,0 +1,49 @@
+import { fastifyCors } from "@fastify/cors";
+import { fastifyMultipart } from "@fastify/multipart";
+import { fastifySwagger } from "@fastify/swagger";
+import { fastifySwaggerUi } from "@fastify/swagger-ui";
+import { fastify } from "fastify";
+import {
+	hasZodFastifySchemaValidationErrors,
+	jsonSchemaTransform,
+	serializerCompiler,
+	validatorCompiler,
+} from "fastify-type-provider-zod";
+
+const app = fastify();
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.setErrorHandler((error, request, reply) => {
+	if (hasZodFastifySchemaValidationErrors(error)) {
+		return reply.status(400).send({
+			message: "Validation error",
+			issues: error.validation,
+		});
+	}
+
+	console.error(error);
+
+	return reply.status(500).send({ message: "Internal app error." });
+});
+
+app.register(fastifyCors, {
+	origin: "*",
+	methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+});
+app.register(fastifyMultipart);
+app.register(fastifySwagger, {
+	openapi: {
+		info: {
+			title: "Brev.ly",
+			version: "0.0.1",
+		},
+	},
+	transform: jsonSchemaTransform,
+});
+app.register(fastifySwaggerUi, {
+	routePrefix: "/docs",
+});
+
+export { app };
