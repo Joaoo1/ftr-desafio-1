b905d006beaaa02082dc9ac8a4316909b65b672a
João <silva.joao1@outlook.com.br>
2025-05-27 00:37:48 -0300

    feat(web): add routing with react-router and create Home page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,9 @@
+import { Router } from "./Router";
+
 export function App() {
 	return (
-		<div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-			<h1 className="text-center">Hello brevly</h1>
-		</div>
+		<>
+			<Router />
+		</>
 	);
 }
